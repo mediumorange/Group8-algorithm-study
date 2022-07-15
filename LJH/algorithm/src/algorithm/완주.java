ff84289de80b8f8a34ce984f9b8f3f8b72aa99cf
Jheosu <little8770@naver.com>
2022-07-16 01:20:04 +0900

    20번 upgrade

--- LJH/algorithm/src/algorithm/완주.java
+++ LJH/algorithm/src/algorithm/완주.java
@@ -1,43 +1,64 @@
 package algorithm;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class øœ¡÷ {
 	
 	public static String solution(String[] participant, String[] completion) {
-		String answer = "";
-
 		
-		ArrayList<String> par = new ArrayList<String>(Arrays.asList(participant));
-        ArrayList<String> com= new ArrayList<String>(Arrays.asList(completion));
- 
-    
-        
-        for(String i : com) {
+		String answer = "";
+        Map<String, Integer> par = new HashMap<>();
+        int target = 1;
+		
+        for (int i =0;i<participant.length; i++) {
+        	String name = participant[i];
+        	if (par.containsKey(name)) {
+        		target = par.get(name);
+        		par.put(name,++target);
+        	}     	
         	
-        	par.remove(i);
+        	else {
+        		par.put(participant[i],1);	
+        	}
+        }
+        for (int i = 0; i<completion.length;i++) {
+        	String name = completion[i];
+        	target = par.get(name);
+        	System.out.println("par"+par);
+        	if (target == 1) {
+        		par.remove(name);
+        	}
+        	else {
+        		par.put(name,--target);
+        		
+        	}
+        	
         	
         }
         
-       
-        answer = par.get(0);
+        Set<String>keys = par.keySet();
+        String key = keys.toString();
+        answer = key.substring(1,key.length()-1);
+        
         return answer;
 
         
     }
 
 	public static void main(String[] args) {
-        String[] par = {"a","a","c","d"};
-        String[] com = {"a","d","c"};
+		
+        String[] par = {"a","b","c"};
+        String[] com = {"a","b"};
        
 		
 
-		
 		System.out.println(solution(par,com));
 		
-        
+   
 		
 		  
 	}
