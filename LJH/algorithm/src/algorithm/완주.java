b2d6818a3c8abafff60bc78279faf1d7b76ae86e
Jheosu <little8770@naver.com>
2022-07-15 23:05:43 +0900

    20

--- LJH/algorithm/src/algorithm/완주.java
+++ LJH/algorithm/src/algorithm/완주.java
@@ -2,8 +2,9 @@ package algorithm;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Collections;
 
-public class 완주하지못한선수 {
+public class 완주 {
 	
 	public static String solution(String[] participant, String[] completion) {
 		String answer = "";
@@ -11,27 +12,26 @@ public class 완주하지못한선수 {
 		
 		ArrayList<String> par = new ArrayList<String>(Arrays.asList(participant));
         ArrayList<String> com= new ArrayList<String>(Arrays.asList(completion));
-
+ 
+    
         
-        for (String i : com) {
-        	if (par.contains(i)) {
-        		par.remove(i);
-        
-        	}
+        for(String i : com) {
         	
-        
+        	par.remove(i);
+        	
         }
         
+       
         answer = par.get(0);
-
         return answer;
 
         
     }
 
 	public static void main(String[] args) {
-        String[] par = {"a","b","a","c"};
-        String[] com = {"b","c","a"};
+        String[] par = {"a","a","c","d"};
+        String[] com = {"a","d","c"};
+       
 		
 
 		
