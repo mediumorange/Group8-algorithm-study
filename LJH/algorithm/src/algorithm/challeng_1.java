d7b390d3c7404fc579bb4eeec583d606f5cc6d04
Jheosu <little8770@naver.com>
2022-07-16 14:55:36 +0900

    challenge 1 upgrade

--- LJH/algorithm/src/algorithm/challeng_1.java
+++ LJH/algorithm/src/algorithm/challeng_1.java
@@ -4,35 +4,18 @@ import java.util.Collections;
 public class challeng_1 {
 	
 	 public static int solution(int n) {
-	        int answer = 0;
-	        ArrayList<Integer> arrList = new ArrayList<Integer>();
-	        if (n == 1) {
-	        	return 1;
-	        } 
-	        while (!(n/3 < 3)) {
-	        	arrList.add(0,n%3);
-	        	n/=3;
-	        	if (n/3 < 3) {
-		        	arrList.add(0,n%3);
-	        		arrList.add(0,n/3);
-	        	}
-	        	
+	        String a = "";
+	        while (n > 0) {
+	        	a = (n%3) + a;
+	        	n /= 3;
 	        }
+	        a = new StringBuffer(a).reverse().toString();       
 	        
-	        Collections.reverse(arrList);
-	        System.out.println(arrList);
-	        int index = arrList.size()-1;
-	        
-	        for (int i : arrList) {
-	        	answer += i*Math.pow(3, index);
-	        	--index;
-	        }  
-	        return answer;
+	        return Integer.parseInt(a,3);
 	
 	 }
 	public static void main(String[] args) {
-		
-		System.out.println(solution(1));
+		System.out.println(solution(25));
 		
 
 	}
