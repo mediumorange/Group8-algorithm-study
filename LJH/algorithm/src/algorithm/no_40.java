ff64bf30680481da666eb2f36fba03238e3b63e5
Jheosu <little8770@naver.com>
2022-07-20 16:51:13 +0900

    no.40 update

--- LJH/algorithm/src/algorithm/no_40.java
+++ LJH/algorithm/src/algorithm/no_40.java
@@ -3,30 +3,20 @@ package algorithm;
 public class no_40 {
     public String solution(String new_id) {
         String answer = "";
-        String s = "[^a-z\\d\\-_.]*";
-        answer = new_id.toLowerCase();                     //case1
-        answer = answer.replaceAll(s,"");       //case2
-        answer = answer.replaceAll("\\.{2,}",".");  //case3
-        StringBuffer ans = new StringBuffer(answer);
+        answer = new_id.toLowerCase();      //case1
+        answer = answer
+                .replaceAll("[^a-z\\d\\-_.]*","")    //case2
+                .replaceAll("\\.{2,}",".")           //case3 z-.
+                .replaceAll("^[.]|[.]$","");                //case4
 
-        if(ans.charAt(0) == '.' ) {   //case4
-            ans.deleteCharAt(0);
+        if(answer.length() == 0) {   //case5
+            answer = "a";
         }
-
-
-        if(ans.length() == 0) {
-            ans.append("a");
-        }
-        else if (ans.charAt(ans.length()-1) == '.') {
-            ans = ans.deleteCharAt(ans.length()-1);
-        }
-
-        answer = ans.toString();
 
         if(answer.length()>15) {             //case6
             answer =answer.substring(0,15);
             if (answer.charAt(answer.length()-1) == '.') {
-                answer = answer.substring(0,14);
+                answer = answer.replaceAll(".$","");
             }
         }
 
@@ -41,6 +31,6 @@ public class no_40 {
 
     public static void main(String[] args) {
         no_40 so = new no_40();
-        System.out.println(so.solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(so.solution("abcdefghijklmn.p"));
     }
 }
